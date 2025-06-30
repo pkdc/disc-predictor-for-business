9d20d186c52bc43473aaac041d71303653131b1a
pkdc <pkdchoy@gmail.com>
2025-06-30 14:58:02 +0100

    use url for bert for deployment

--- utils/bert_embedder.py
+++ utils/bert_embedder.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 import tensorflow_text
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-bert_preprocess = hub.KerasLayer(os.path.join(base_dir, "..", "tfhub_modules", "bert_preprocess"), trainable=False)
-bert_encoder = hub.KerasLayer(os.path.join(base_dir, "..", "tfhub_modules", "bert_encoder"), trainable=False)
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+# bert_preprocess = hub.KerasLayer(os.path.join(base_dir, "..", "tfhub_modules", "bert_preprocess"), trainable=False)
+# bert_encoder = hub.KerasLayer(os.path.join(base_dir, "..", "tfhub_modules", "bert_encoder"), trainable=False)
 
-# bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3", trainable=False)
-# bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3", trainable=False)
+bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3", trainable=False)
+bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3", trainable=False)
 
 def get_bert_embeddings(texts):
     text_inputs = tf.constant(texts)
